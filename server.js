0cdebf8d9bbbddb3c9ec0c5393525fef8b985744
Josiah Maina <josiah5115@gmail.com>
2018-07-17 10:50:45 -0600

    modified server file

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ const app = express();
 
 let employees = ['Jim', 'Pam', 'Dwight', 'Rayan', 'Creed', 'Angela'];
 
-app.get(
-  'api/employees', 
-  (req, res, next) => {
-  if(req.query.username === 'mscott') {
-    next();
-  } else {
-    res.status(403).send('nope')
-  }
-  },
-  (req, res) => {
-    res.status(200).send(employees);
-  }
-};
+app.get('/api/employees',
+//request level middleware
+ (req, res, next) => {
+    if (req.query.username === 'mscott') {
+        next();
+    } else {
+        res.status(403).send('nope')
+    }
+});
+
+app.use(bodyParser.json())
+
+app.get('/api/employees', (req, res) => {
+    res.status(200).send('employees');
+});
+
+app.get('/api/test', (req, res) => {
+    res.status(200).send('secret data');
+});
 
 const port = 3030;
 app.listen(port, () => {
